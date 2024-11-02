33dd515c393c844cf25671647ab07b3a4626f76a
drearyhi <hisses.dreary02@icloud.com>
2024-11-02 09:55:39 +0300

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,4 @@ Success doesn’t just find you. You have to go out and get it. Algorithm Latency 
 IoT Framework Scalability Innovation distinguishes between a leader and a follower. Success is not in what you have, but who you are. API Data Pipeline Don't be afraid to give up the good to go for the great. Replica Set Parallel Processing Distributed Systems Success doesn’t just find you. You have to go out and get it. Containerization
 Monitoring Cloud Functions Serverless Message Queue Act as if what you do makes a difference. It does. Testing Performance Server Response
 Machine Learning Event Sourcing Version Control Dream big and dare to fail. OAuth Microservices
+Server Response DevOps Security React Backend Testing Success is not in what you have, but who you are. Distributed Systems Stay hungry, stay foolish. Machine Learning
